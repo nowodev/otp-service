cc8ce992971714d2a9435fbbc4a340215f791ccd
Favour <toluwaniokunowo@gmail.com>
2022-06-08 11:27:47 +0100

    removing existing otp per phone number

--- app/Actions/GenerateOTP.php
+++ app/Actions/GenerateOTP.php
@@ -3,12 +3,15 @@
 namespace App\Actions;
 
 use App\Models\Otp;
+use Illuminate\Support\Facades\DB;
 
 class GenerateOTP
 {
     public function handle(string $phone_number)
     {
         $otp = mt_rand(100000, 999999);
+
+        Otp::query()->where('phone_number', $phone_number)->delete();
 
         Otp::query()->create([
             'phone_number' => $phone_number,
